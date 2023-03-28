5ab5f5dd56e70384d3caabddffeb44b52c767931
Kaushal Lodd <kaushallodd62@gmail.com>
2023-03-28 13:47:53 +0530

    formatted smart contracts

--- contracts/EnergyMarket.sol
+++ contracts/EnergyMarket.sol
@@ -4,13 +4,12 @@ pragma solidity ^0.8.18;
 import "./NRGToken.sol";
 
 contract EnergyMarket {
-    
     // state variables
     NRGToken private immutable tokenContract;
     uint256 private numberOfOffers;
     uint256 private numberOfDemands;
     address public immutable owner;
-    
+
     // structures
     struct Offer {
         uint256 idOffer;
@@ -24,153 +23,245 @@ contract EnergyMarket {
         string nameBuyer;
         uint256 energyDemanded;
     }
-    
+
     // mappings
     mapping(uint256 => Demand) private demands;
     mapping(uint256 => Offer) private offers;
 
     // events
-    event OfferPlaced(uint256 idOffer, address addrSeller, string nameSeller, uint256 energyOffered);
-    event DemandPlaced(uint256 idDemand, address addrBuyer, string nameBuyer, uint256 energyDemanded);
-    event OfferRevoked(uint256 idOffer, address addrSeller, string nameSeller, uint256 energyOffered);
-    event DemandRevoked(uint256 idDemand, address addrBuyer, string nameBuyer, uint256 energyDemanded);
-    event DealMade(uint256 idOffer, uint256 idDemand, address addrSeller, string nameSeller, uint256 energyOffered, address addrBuyer, string nameBuyer, uint256 energyDemanded, uint256 NRGprice);
-    
+    event OfferPlaced(
+        uint256 idOffer,
+        address addrSeller,
+        string nameSeller,
+        uint256 energyOffered
+    );
+    event DemandPlaced(
+        uint256 idDemand,
+        address addrBuyer,
+        string nameBuyer,
+        uint256 energyDemanded
+    );
+    event OfferRevoked(
+        uint256 idOffer,
+        address addrSeller,
+        string nameSeller,
+        uint256 energyOffered
+    );
+    event DemandRevoked(
+        uint256 idDemand,
+        address addrBuyer,
+        string nameBuyer,
+        uint256 energyDemanded
+    );
+    event DealMade(
+        uint256 idOffer,
+        uint256 idDemand,
+        address addrSeller,
+        string nameSeller,
+        uint256 energyOffered,
+        address addrBuyer,
+        string nameBuyer,
+        uint256 energyDemanded,
+        uint256 NRGprice
+    );
+
     // constructor
-    constructor (NRGToken _tokenContract)  {
-        numberOfOffers=0;
-        numberOfDemands=0;
-        tokenContract=_tokenContract;
-        owner=msg.sender;
+    constructor(NRGToken _tokenContract) {
+        numberOfOffers = 0;
+        numberOfDemands = 0;
+        tokenContract = _tokenContract;
+        owner = msg.sender;
     }
 
     // Get the current user
     function getCurrentUser() public view returns (address) {
         return msg.sender;
     }
-    
+
     // Producer places an offer
-    function placeOffer (address _addrSeller, string memory _nameSeller, uint256 _energyOffered) public {
-        require(msg.sender == _addrSeller, "Offer can be placed only by the user calling this function");
+    function placeOffer(
+        address _addrSeller,
+        string memory _nameSeller,
+        uint256 _energyOffered
+    ) public {
+        require(
+            msg.sender == _addrSeller,
+            "Offer can be placed only by the user calling this function"
+        );
         require(_energyOffered > 0, "Energy offered must be greater than 0");
         Offer memory o;
-        o.addrSeller=_addrSeller;
-        o.nameSeller=_nameSeller;
-        o.energyOffered=_energyOffered;
-        o.idOffer= numberOfOffers;
-        offers[numberOfOffers]=o;
+        o.addrSeller = _addrSeller;
+        o.nameSeller = _nameSeller;
+        o.energyOffered = _energyOffered;
+        o.idOffer = numberOfOffers;
+        offers[numberOfOffers] = o;
         numberOfOffers++;
-        emit OfferPlaced(o.idOffer, o.addrSeller, o.nameSeller, o.energyOffered);
+        emit OfferPlaced(
+            o.idOffer,
+            o.addrSeller,
+            o.nameSeller,
+            o.energyOffered
+        );
     }
-  
+
     // Consumer places a demand
-    function placeDemand (address _addrBuyer, string memory _nameBuyer, uint256 _energyDemanded) public {
-        require(msg.sender == _addrBuyer, "Demand can be placed only by the user calling this function");
+    function placeDemand(
+        address _addrBuyer,
+        string memory _nameBuyer,
+        uint256 _energyDemanded
+    ) public {
+        require(
+            msg.sender == _addrBuyer,
+            "Demand can be placed only by the user calling this function"
+        );
         require(_energyDemanded > 0, "Energy demanded must be greater than 0");
         Demand memory d;
-        d.addrBuyer=_addrBuyer;
-        d.nameBuyer=_nameBuyer;
-        d.energyDemanded=_energyDemanded;
-        d.idDemand= numberOfDemands;
-        demands[numberOfDemands]=d;
+        d.addrBuyer = _addrBuyer;
+        d.nameBuyer = _nameBuyer;
+        d.energyDemanded = _energyDemanded;
+        d.idDemand = numberOfDemands;
+        demands[numberOfDemands] = d;
         numberOfDemands++;
-        emit DemandPlaced(d.idDemand, d.addrBuyer, d.nameBuyer, d.energyDemanded);
+        emit DemandPlaced(
+            d.idDemand,
+            d.addrBuyer,
+            d.nameBuyer,
+            d.energyDemanded
+        );
     }
 
     // Revoke an offer
-    function revokeOffer (uint256 _idOffer, address _addr) public {
+    function revokeOffer(uint256 _idOffer, address _addr) public {
         Offer memory o = offers[_idOffer];
-        require(o.addrSeller == _addr, "Address provided doesn't match the address of the user who placed the offer");
-        require(msg.sender == o.addrSeller || msg.sender == owner, "Offer can be revoked only by the user who placed it or by the owner of the contract");
+        require(
+            o.addrSeller == _addr,
+            "Address provided doesn't match the address of the user who placed the offer"
+        );
+        require(
+            msg.sender == o.addrSeller || msg.sender == owner,
+            "Offer can be revoked only by the user who placed it or by the owner of the contract"
+        );
         delete offers[_idOffer];
-        for(uint i=_idOffer;i<numberOfOffers-1;i++){
-            offers[i]=offers[i+1];
+        for (uint i = _idOffer; i < numberOfOffers - 1; i++) {
+            offers[i] = offers[i + 1];
         }
-        offers[numberOfOffers-1]=Offer(0,address(0),"",0);
+        offers[numberOfOffers - 1] = Offer(0, address(0), "", 0);
         numberOfOffers--;
-        emit OfferRevoked(_idOffer, o.addrSeller, o.nameSeller, o.energyOffered);
+        emit OfferRevoked(
+            _idOffer,
+            o.addrSeller,
+            o.nameSeller,
+            o.energyOffered
+        );
     }
 
     // Revoke a demand
-    function revokeDemand (uint256 _idDemand, address _addr) public {
+    function revokeDemand(uint256 _idDemand, address _addr) public {
         Demand memory d = demands[_idDemand];
-        require(d.addrBuyer == _addr, "Address provided doesn't match the address of the user who placed the demand");
-        require(msg.sender == d.addrBuyer || msg.sender == owner, "Demand can be revoked only by the user who placed it or by the owner of the contract");
+        require(
+            d.addrBuyer == _addr,
+            "Address provided doesn't match the address of the user who placed the demand"
+        );
+        require(
+            msg.sender == d.addrBuyer || msg.sender == owner,
+            "Demand can be revoked only by the user who placed it or by the owner of the contract"
+        );
         delete demands[_idDemand];
-        for(uint i=_idDemand;i<numberOfDemands-1;i++){
-            demands[i]=demands[i+1];
+        for (uint i = _idDemand; i < numberOfDemands - 1; i++) {
+            demands[i] = demands[i + 1];
         }
-        demands[numberOfDemands-1]=Demand(0,address(0),"",0);
+        demands[numberOfDemands - 1] = Demand(0, address(0), "", 0);
         numberOfDemands--;
-        emit DemandRevoked(_idDemand, d.addrBuyer, d.nameBuyer, d.energyDemanded);
+        emit DemandRevoked(
+            _idDemand,
+            d.addrBuyer,
+            d.nameBuyer,
+            d.energyDemanded
+        );
     }
-    
+
     // Get the number of offers
     function getOffersLength() public view returns (uint256) {
-        return(numberOfOffers);
+        return (numberOfOffers);
     }
-    
+
     // Get the number of demands
     function getDemandsLength() public view returns (uint256) {
-        return(numberOfDemands);
+        return (numberOfDemands);
     }
 
     // Get the offer by id
-    function getOfferById(uint256 _id) public view returns (address, string memory, uint256){
-        return (offers[_id].addrSeller, offers[_id].nameSeller, offers[_id].energyOffered);
+    function getOfferById(
+        uint256 _id
+    ) public view returns (address, string memory, uint256) {
+        return (
+            offers[_id].addrSeller,
+            offers[_id].nameSeller,
+            offers[_id].energyOffered
+        );
     }
-    
+
     // Get the demand by id
-    function getDemandById(uint256 _id) public view returns (address, string memory, uint256){
-        return (demands[_id].addrBuyer, demands[_id].nameBuyer, demands[_id].energyDemanded);
+    function getDemandById(
+        uint256 _id
+    ) public view returns (address, string memory, uint256) {
+        return (
+            demands[_id].addrBuyer,
+            demands[_id].nameBuyer,
+            demands[_id].energyDemanded
+        );
     }
 
     // Get all offers by address
-    function getOffersByAddress(address _addr) public view returns (Offer[] memory) {
+    function getOffersByAddress(
+        address _addr
+    ) public view returns (Offer[] memory) {
         Offer[] memory offersTab = new Offer[](numberOfOffers);
-        uint256 j=0;
-        
-        for(uint i=0;i<numberOfOffers;i++){
-            if(offers[i].addrSeller==_addr){
-                offersTab[j]=offers[i];
+        uint256 j = 0;
+
+        for (uint i = 0; i < numberOfOffers; i++) {
+            if (offers[i].addrSeller == _addr) {
+                offersTab[j] = offers[i];
                 j++;
             }
         }
-        return(offersTab);
+        return (offersTab);
     }
 
     // Get all demands by address
-    function getDemandsByAddress(address _addr) public view returns (Demand[] memory) {
+    function getDemandsByAddress(
+        address _addr
+    ) public view returns (Demand[] memory) {
         Demand[] memory demandsTab = new Demand[](numberOfDemands);
-        uint256 j=0;
-        
-        for(uint i=0;i<numberOfDemands;i++){
-            if(demands[i].addrBuyer==_addr){
-                demandsTab[j]=demands[i];
+        uint256 j = 0;
+
+        for (uint i = 0; i < numberOfDemands; i++) {
+            if (demands[i].addrBuyer == _addr) {
+                demandsTab[j] = demands[i];
                 j++;
             }
         }
-        return(demandsTab);
+        return (demandsTab);
     }
-    
+
     // Get all offers
     function getAllOffers() public view returns (Offer[] memory) {
         Offer[] memory offersTab = new Offer[](numberOfOffers);
-        
-        for(uint i=0;i<numberOfOffers;i++){
-            offersTab[i]=offers[i];
+
+        for (uint i = 0; i < numberOfOffers; i++) {
+            offersTab[i] = offers[i];
         }
-        return(offersTab);
+        return (offersTab);
     }
 
     // Get all demands
     function getAllDemands() public view returns (Demand[] memory) {
         Demand[] memory demandsTab = new Demand[](numberOfDemands);
-        
-        for(uint i=0;i<numberOfDemands;i++){
-            demandsTab[i]=demands[i];
+
+        for (uint i = 0; i < numberOfDemands; i++) {
+            demandsTab[i] = demands[i];
         }
-        return(demandsTab);
+        return (demandsTab);
     }
 
     // Make a deal
@@ -181,23 +272,42 @@ contract EnergyMarket {
         address addrBuyer = demands[_idDemand].addrBuyer;
         string memory nameBuyer = demands[_idDemand].nameBuyer;
         uint256 energyDemanded = demands[_idDemand].energyDemanded;
-        require(msg.sender == addrSeller || msg.sender == addrBuyer, "Deal can be made only by the buyer or the seller");
-        require(energyOffered == energyDemanded, "Energy offered must be equal to energy demanded");
+        require(
+            msg.sender == addrSeller || msg.sender == addrBuyer,
+            "Deal can be made only by the buyer or the seller"
+        );
+        require(
+            energyOffered == energyDemanded,
+            "Energy offered must be equal to energy demanded"
+        );
         uint256 NRGprice = energyOffered;
-        require(tokenContract.balanceOf(addrBuyer) >= NRGprice * (10**18), "Buyer doesn't have enough NRG tokens");
+        require(
+            tokenContract.balanceOf(addrBuyer) >= NRGprice * (10 ** 18),
+            "Buyer doesn't have enough NRG tokens"
+        );
         tokenContract.transferFrom(addrBuyer, addrSeller, NRGprice);
         delete offers[_idOffer];
-        for(uint i=_idOffer;i<numberOfOffers-1;i++){
-            offers[i]=offers[i+1];
+        for (uint i = _idOffer; i < numberOfOffers - 1; i++) {
+            offers[i] = offers[i + 1];
         }
-        offers[numberOfOffers-1]=Offer(0,address(0),"",0);
+        offers[numberOfOffers - 1] = Offer(0, address(0), "", 0);
         numberOfOffers--;
         delete demands[_idDemand];
-        for(uint i=_idDemand;i<numberOfDemands-1;i++){
-            demands[i]=demands[i+1];
+        for (uint i = _idDemand; i < numberOfDemands - 1; i++) {
+            demands[i] = demands[i + 1];
         }
-        demands[numberOfDemands-1]=Demand(0,address(0),"",0);
+        demands[numberOfDemands - 1] = Demand(0, address(0), "", 0);
         numberOfDemands--;
-        emit DealMade(_idOffer, _idDemand, addrSeller, nameSeller, energyOffered, addrBuyer, nameBuyer, energyDemanded, NRGprice);
+        emit DealMade(
+            _idOffer,
+            _idDemand,
+            addrSeller,
+            nameSeller,
+            energyOffered,
+            addrBuyer,
+            nameBuyer,
+            energyDemanded,
+            NRGprice
+        );
     }
 }
